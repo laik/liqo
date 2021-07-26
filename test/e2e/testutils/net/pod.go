62dfefc43c9bf6219b775fe7f61af3432875af3f
Alex Palesandro <alex.palesandro@gmail.com>
2021-07-26 11:44:46 +0000

    test/e2e/pipeline/Refactor E2E Tests

--- test/e2e/testutils/net/pod.go
+++ test/e2e/testutils/net/pod.go
@@ -2,6 +2,7 @@ package net
 
 import (
 	"context"
+	"strings"
 
 	v1 "k8s.io/api/core/v1"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
@@ -9,24 +10,23 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/klog/v2"
 
-	liqoconst "github.com/liqotech/liqo/pkg/consts"
 	"github.com/liqotech/liqo/test/e2e/testutils/util"
 )
 
 // EnsureNetTesterPods creates the NetTest pods and waits for them to be ready.
-func EnsureNetTesterPods(ctx context.Context, homeClient kubernetes.Interface, homeID string) error {
+func EnsureNetTesterPods(ctx context.Context, homeClient kubernetes.Interface, homeID, remoteID string) error {
 	ns, err := util.EnforceNamespace(ctx, homeClient, homeID, TestNamespaceName)
 	if err != nil && !kerrors.IsAlreadyExists(err) {
 		klog.Error(err)
 		return err
 	}
-	podRemote := forgeTesterPod(image, podTesterRemoteCl, ns.Name, true)
+	podRemote := forgeTesterPod(image, podTesterRemoteCl, ns.Name, true, remoteID)
 	_, err = homeClient.CoreV1().Pods(ns.Name).Create(ctx, podRemote, metav1.CreateOptions{})
 	if err != nil && !kerrors.IsAlreadyExists(err) {
 		klog.Error(err)
 		return err
 	}
-	podLocal := forgeTesterPod(image, podTesterLocalCl, ns.Name, false)
+	podLocal := forgeTesterPod(image, podTesterLocalCl, ns.Name, false, "")
 	_, err = homeClient.CoreV1().Pods(ns.Name).Create(ctx, podLocal, metav1.CreateOptions{})
 	if err != nil && !kerrors.IsAlreadyExists(err) {
 		klog.Error(err)
@@ -44,10 +44,10 @@ func CheckTesterPods(ctx context.Context, homeClient, foreignClient kubernetes.I
 }
 
 // forgeTesterPod deploys the Remote pod of the test.
-func forgeTesterPod(image, podName, namespace string, isRemote bool) *v1.Pod {
-	NodeAffinityOperator := v1.NodeSelectorOpNotIn
+func forgeTesterPod(image, podName, namespace string, isRemote bool, remoteID string) *v1.Pod {
+	nodeName := ""
 	if isRemote {
-		NodeAffinityOperator = v1.NodeSelectorOpIn
+		nodeName = strings.Join([]string{"liqo", remoteID}, "-")
 	}
 
 	pod1 := v1.Pod{
@@ -69,18 +69,7 @@ func forgeTesterPod(image, podName, namespace string, isRemote bool) *v1.Pod {
 					}},
 				},
 			},
-			Affinity: &v1.Affinity{
-				NodeAffinity: &v1.NodeAffinity{
-					RequiredDuringSchedulingIgnoredDuringExecution: &v1.NodeSelector{NodeSelectorTerms: []v1.NodeSelectorTerm{{
-						MatchExpressions: []v1.NodeSelectorRequirement{{
-							Key:      liqoconst.TypeLabel,
-							Operator: NodeAffinityOperator,
-							Values:   []string{liqoconst.TypeNode},
-						}},
-						MatchFields: nil,
-					}}},
-				},
-			},
+			NodeName: nodeName,
 		},
 		Status: v1.PodStatus{},
 	}
